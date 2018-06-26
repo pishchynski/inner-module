c55b77efaad7a55ae09cef77f271eb08292794f0
pishchynski <pavelalexbel@outlook.com>
2018-06-26 17:48:24 +0300

    initial

---
+++ src/by/pavelbel/SomeInnerStuff.java
@@ -0,0 +1,17 @@
+package by.pavelbel;
+
+public class SomeInnerStuff {
+    private String name;
+
+    SomeInnerStuff() {
+        this.name = "Default";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
